4aad2a8719bfdc7fbfd0db1b302d3dac50e8876c
Hanjaya Suryalim <hanjayasuryalim@gmail.com>
2020-07-14 21:11:01 +0700

    set up layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black45,
-      body: SafeArea(
-        child: QuizPage(),
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
       ),
-    ),
-  ));
+    );
+  }
 }
 
 class QuizPage extends StatefulWidget {
@@ -20,21 +31,46 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Text('This is where the question text will go'),
-        FlatButton(
-          color: Colors.green,
-          onPressed: () {},
-          child: Center(
-            child: Text('True'),
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where question text will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              ),
+            ),
           ),
         ),
-        FlatButton(
-          color: Colors.red,
-          onPressed: () {},
-          child: Center(
-            child: Text('False'),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              onPressed: () {},
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: FlatButton(
+              onPressed: () {},
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
           ),
         )
       ],

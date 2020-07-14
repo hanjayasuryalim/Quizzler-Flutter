67b57b02328aa567b89bfa0e5060d17a17cff917
Hanjaya Suryalim <hanjayasuryalim@gmail.com>
2020-07-15 01:59:20 +0700

    encapsulate quiz functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(Quizzler());
@@ -31,17 +33,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   // variables
   List<Icon> iconList = [];
-  List<Question> questionList = [
-    Question(
-        question: 'You can lead a cow down stairs but not up stairs.',
-        answer: false),
-    Question(
-        question: 'Approximately one quarter of human bones are in the feet',
-        answer: true),
-    Question(question: 'A slug\'s blood is green', answer: true)
-  ];
 
-  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -54,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionList[questionNumber].questionText,
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.white, fontSize: 25.0),
               ),
@@ -66,13 +58,13 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               onPressed: () {
-                if (questionList[questionNumber].questionAnswer == true) {
+                if (quizBrain.getAnswer() == true) {
                   print('correct');
                 } else {
                   print('incorrect');
                 }
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
               color: Colors.green,
@@ -88,13 +80,13 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: FlatButton(
               onPressed: () {
-                if (questionList[questionNumber].questionAnswer == false) {
+                if (quizBrain.getAnswer() == false) {
                   print('correct');
                 } else {
                   print('incorrect');
                 }
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
               color: Colors.red,
